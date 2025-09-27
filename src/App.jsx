3b4e1754b3ff55fec652d004a08b574b89d12354
jbriones2901-art <jbriones.2901@gmail.com>
2025-09-27 12:21:01 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-[#f4f4f9] min-h-screen flex flex-col items-center justify-center text-gray-800">
+      <h1 className="text-4xl font-bold mb-6">你好，Kumo！</h1>
+      <p className="text-xl mb-2">你是最特别的人。</p>
+      <p className="text-xl mb-2">愿你的笑容永远灿烂。</p>
+      <p className="text-xl mb-2">友谊如星辰，永远闪耀。</p>
+      <p className="text-xl">F1 的速度如同你带来的激情。</p>
+    </div>
+  )
+}
+
+export default App
